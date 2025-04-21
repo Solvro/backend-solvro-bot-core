fd98a4bd21dbf9a64b1a40af86e241aacee2c666
Adimac93 <adimac93@gmail.com>
2025-04-21 20:22:19 +0200

    docs: add autoswagger

--- start/routes.ts
+++ start/routes.ts
@@ -6,14 +6,24 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
-
 import router from '@adonisjs/core/services/router'
+import AutoSwagger from 'adonis-autoswagger'
+import swagger from '#config/swagger'
+
 const RecordingsController = () => import('#controllers/recordings_controller')
 
 router.get('/', async () => {
   return {
     hello: 'world',
   }
+})
+
+router.get('/swagger', async () => {
+  return AutoSwagger.default.docs(router.toJSON(), swagger)
+})
+
+router.get('/docs', async () => {
+  return AutoSwagger.default.scalar('/swagger')
 })
 
 router.patch('/recordings/:id', [RecordingsController, 'register'])

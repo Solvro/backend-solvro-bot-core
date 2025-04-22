a593130cb75ec52dd79016da9303c8bf0e304768
Adimac93 <adimac93@gmail.com>
2025-04-22 19:53:53 +0200

    feat(transcriber): save recording metadata

--- app/controllers/recordings_controller.ts
+++ app/controllers/recordings_controller.ts
@@ -1,25 +1,46 @@
 import Meeting, { RecordingStatus } from '#models/meetings'
-import { updateRecordingValidator } from '#validators/recording'
+import { updateMeetingValidator } from '#validators/recording'
 import type { HttpContext } from '@adonisjs/core/http'
+import db from '@adonisjs/lucid/services/db'
 import vine from '@vinejs/vine'
-import { DateTime } from 'luxon'
+import { DateTime, Duration } from 'luxon'
 
 export default class RecordingsController {
   async register({ request, params, response }: HttpContext) {
     const schema = vine.object({ id: vine.number() })
     const validator = await vine.compile(schema)
     const { id } = await validator.validate(params)
-    const payload = await request.validateUsing(updateRecordingValidator)
-    const meetings = await Meeting.find(id)
-    if (!meetings) {
-      return response.status(404).json({ message: 'Recording not found' })
+    const payload = await request.validateUsing(updateMeetingValidator)
+
+    const trx = await db.transaction()
+    try {
+      const meeting = await Meeting.find(id)
+      if (!meeting) {
+        return response.status(404).json({ message: 'Recording not found' })
+      }
+
+      meeting.transcription = payload.transcription
+      meeting.recordingStatus = RecordingStatus.COMPLETED
+      meeting.finishedAt = DateTime.now()
+      await meeting.useTransaction(trx).save()
+
+      for (const chunk of payload.metadata) {
+        await meeting
+          .useTransaction(trx)
+          .related('chunks')
+          .create({
+            discordUserId: chunk.userId,
+            recordedAt: DateTime.fromMillis(chunk.globalTimestamp),
+            recordingTimestamp: DateTime.fromMillis(chunk.recordingTImestamp),
+            duration: Duration.fromMillis(chunk.duration),
+          })
+      }
+
+      await trx.commit()
+      return response.status(204)
+    } catch (error) {
+      await trx.rollback()
+      throw error
     }
-
-    meetings.transcription = payload.transcription
-    meetings.recordingStatus = RecordingStatus.COMPLETED
-    meetings.finishedAt = DateTime.now()
-    await meetings.save()
-
-    return response.status(204)
   }
 }
--- app/validators/recording.ts
+++ app/validators/recording.ts
@@ -1,7 +1,16 @@
 import vine from '@vinejs/vine'
 
-export const updateRecordingValidator = vine.compile(
+export const updateMeetingValidator = vine.compile(
   vine.object({
     transcription: vine.string(),
+    metadata: vine.array(
+      vine.object({
+        filepath: vine.string(),
+        userId: vine.string(),
+        globalTimestamp: vine.number(),
+        recordingTImestamp: vine.number(),
+        duration: vine.number(),
+      })
+    ),
   })
 )

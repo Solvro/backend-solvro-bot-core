d457f03bedc0edd84700367639e8f69e45c1d42f
loudsheep <loudsheep05@gmail.com>
2025-05-21 13:14:10 +0200

    refactor: change route

--- start/routes.ts
+++ start/routes.ts
@@ -32,4 +32,4 @@ router.get('/health', [HealthChecksController])
 
 router.patch('/recordings/:id', [RecordingsController, 'register'])
 
-router.post('/office/camera/poll', [OfficeCameraController, 'update'])
+router.post('/office/camera', [OfficeCameraController, 'update'])
